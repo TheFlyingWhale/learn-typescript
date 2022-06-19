2ccc2142f2afe34c4d2819d1ca3f30120450a295
Ole Walberg <greblavelo@gmail.com>
2022-06-19 14:24:25 +0200

    Delete build directory

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const classes_1 = __importDefault(require("./exercises/classes"));
-(0, classes_1.default)();
