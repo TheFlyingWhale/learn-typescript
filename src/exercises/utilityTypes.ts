e3d69ca2a3a9f044e4563e84d8f15dc61473bfb8
Ole Walberg <ole@studybud.no>
2022-06-19 16:05:15 +0200

    remove experiment

--- src/exercises/utilityTypes.ts
+++ src/exercises/utilityTypes.ts
@@ -2,17 +2,4 @@ const utilityTypesMain = (): void => {
     console.log("utilityTypes");
 };
 
-interface test {
-    name: string;
-    age: number;
-}
-
-const createObject = (object: object): object => {
-    return {
-        ...object,
-    };
-};
-
-const testObject = createObject<test>({ name: "Ole", age: 26 });
-
 export default utilityTypesMain;
